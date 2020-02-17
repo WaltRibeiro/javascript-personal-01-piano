b8f879d07e62f46f6de7e96664815011f23381c4
WaltRibeiro <walt@fororchestra.com>
2020-02-17 18:55:15 -0500

    cleaner code

--- js/script.js
+++ js/script.js
@@ -114,3 +114,19 @@ $(document).on("click", ".highlight", function() {
 //   var noteDiv = $("#note-" + notesMaryLamb[indexOfArray]);
 //   var noteDiv = $("#note-" + getElementById("#MaryLamb") + [indexOfArray]);
 
+
+
+// Password checker for private work
+var chosenWord = "art";
+
+
+function checkPass() {
+    var promptInput = prompt("Please enter the password to see more work");
+    if (promptInput === chosenWord) {
+        window.location.replace("http://linkedin.waltrib.com");
+    } else if (promptInput != chosenWord) {
+        alert("Incorrect password")
+    } else {
+
+    }
+};
